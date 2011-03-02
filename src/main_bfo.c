ffb718c09e0f96ec0d4578591cc9a9def652f6cf
Fabio Roberto Teodoro <fr.teodoro@gmail.com>
2011-03-02 00:52:08 -0300

    TODOs updated

--- src/main_bfo.c
+++ src/main_bfo.c
@@ -7,14 +7,14 @@ int main(int argc, char *argv[])
 	algorithm_t *bfo_algorithm;
 	int ret;
 
-	parallel_evolution_parse_topology("ring.topology");	/* TODO */
+	parallel_evolution_set_topology_file_name("ring.topology");
 	algorithm_create(&bfo_algorithm,
-			bfo_init,
-			bfo_run_iterations,
-			bfo_insert_migrant,
-			bfo_pick_migrant,
-			bfo_ended,
-			bfo_get_population
+			bfo_init,	/* TODO */
+			bfo_run_iterations,	/* TODO */
+			bfo_insert_migrant,	/* TODO */
+			bfo_pick_migrant,	/* TODO */
+			bfo_ended,	/* TODO */
+			bfo_get_population	/* TODO */
 			);
 	parallel_evolution_add_algorithm(bfo_algorithm, 1, 3);	/* TODO */
 	ret = parallel_evolution_run();	/* TODO */
