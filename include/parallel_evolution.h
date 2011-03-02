f5ee43e4d056678f755398a0219cc21fa30499eb
Fabio Roberto Teodoro <fr.teodoro@gmail.com>
2011-03-01 22:26:15 -0300

    initial version of parallel_evolution_config_t

--- include/parallel_evolution.h
+++ include/parallel_evolution.h
@@ -1,3 +1,18 @@
 #pragma once
 
+#include "common.h"
+#include "topology.h"
+#include "topology_parser.h"
+#include "population.h"
+#include "algorithm.h"
+
+typedef struct parallel_evolution_config {
+	/* used on topology controller */
+	topology_t *topology;
+
+	/* used on algorithm runners */
+	algorithm_t *algorithm;
+} parallel_evolution_config_t;
+
 int parallel_evolution_run(int *argc, char ***argv);
+status_t parallel_evolution_parse_topology(const char *file_name);
