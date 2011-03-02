49e1e0ac176d952e9ffef6f06842720543bfd5a8
Fabio Roberto Teodoro <fr.teodoro@gmail.com>
2011-03-02 01:12:11 -0300

    population_t

--- include/population.h
+++ include/population.h
@@ -1,5 +1,13 @@
 #pragma once
 
-/* TODO empty population stub */
+#include "common.h"
+#include "migrant.h"
+
 typedef struct population {
+	migrant_t **individuals;
+	int size;
 } population_t;
+
+status_t population_create(population_t **population, int size);
+void population_destroy(population_t **population);
+void population_set_individual(population_t *population, migrant_t *migrant, int idx);
---
+++ src/population.c
@@ -0,0 +1,34 @@
+#include "population.h"
+
+#include <stdlib.h>
+
+status_t population_create(population_t **population, int size)
+{
+	population_t *new_population;
+	
+	new_population = (population_t *)malloc(sizeof(population_t));
+	if (new_population == NULL)
+		return FAIL;
+
+	new_population->individuals = (migrant_t **)malloc(size * sizeof(migrant_t *));
+	if (new_population->individuals == NULL) {
+		free(new_population);
+		return FAIL;
+	}
+	new_population->size = size;
+
+	*population = new_population;
+
+	return SUCCESS;
+}
+void population_destroy(population_t **population)
+{
+	free((*population)->individuals);
+	free(*population);
+	*population = NULL;
+}
+
+void population_set_individual(population_t *population, migrant_t *migrant, int idx)
+{
+	population->individuals[idx] = migrant;	/* XXX too bad if idx is bigger than population->size */
+}
